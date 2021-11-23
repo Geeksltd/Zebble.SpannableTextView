077cf19077e3dd2ac3d278c6af91c4dda907b631
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-11-23 12:36:12 +0330

    Release

--- iOS/IOSRenderExtentions.cs
+++ iOS/IOSRenderExtentions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Zebble.IOS
 {
     using Foundation;
+    using Olive;
     using System;
     using UIKit;
 
@@ -8,38 +9,45 @@
     {
         internal static void RenderSpannableStringStyle(this SpannableStringStyle style, TextView view, NSMutableAttributedString attributedString)
         {
-            var range = new NSRange(style.Range.Start, style.Range.Length);
-            switch (style.Type)
+            try
             {
-                case SpannableStringTypes.B:
-                case SpannableStringTypes.Bold:
-                    attributedString.AddAttribute(UIStringAttributeKey.Font, UIFont.BoldSystemFontOfSize(view.Font.Size), range);
-                    break;
-                case SpannableStringTypes.I:
-                case SpannableStringTypes.Italic:
-                    attributedString.AddAttribute(UIStringAttributeKey.Font, UIFont.ItalicSystemFontOfSize(view.Font.Size), range);
-                    break;
-                case SpannableStringTypes.Font:
-                    foreach (var parameter in style.Parameters)
-                    {
-                        switch (parameter.Key)
+                var range = new NSRange(style.Range.Start, style.Range.Length);
+                switch (style.Type)
+                {
+                    case SpannableStringTypes.B:
+                    case SpannableStringTypes.Bold:
+                        attributedString.AddAttribute(UIStringAttributeKey.Font, UIFont.BoldSystemFontOfSize(view.Font.Size), range);
+                        break;
+                    case SpannableStringTypes.I:
+                    case SpannableStringTypes.Italic:
+                        attributedString.AddAttribute(UIStringAttributeKey.Font, UIFont.ItalicSystemFontOfSize(view.Font.Size), range);
+                        break;
+                    case SpannableStringTypes.Font:
+                        foreach (var parameter in style.Parameters)
                         {
-                            case SpannableStringParameterTypes.Size:
-                                if (!float.TryParse(parameter.Value, out float fontSize)) break;
-                                var font = UIFont.FromName(view.Font.Name, fontSize);
-                                if (font == null) break;
-                                attributedString.AddAttribute(UIStringAttributeKey.Font, font, range);
-                                break;
-                            case SpannableStringParameterTypes.Color:
-                                attributedString.AddAttribute(UIStringAttributeKey.ForegroundColor, Color.Parse(parameter.Value).Render(), range);
-                                break;
-                            // TODO implement font faces.
-                            // case SpannableStringParameterTypes.Face: break;
-                            default: break;
+                            switch (parameter.Key)
+                            {
+                                case SpannableStringParameterTypes.Size:
+                                    if (!float.TryParse(parameter.Value, out float fontSize)) break;
+                                    var font = UIFont.FromName(view.Font.Name, fontSize);
+                                    if (font == null) break;
+                                    attributedString.AddAttribute(UIStringAttributeKey.Font, font, range);
+                                    break;
+                                case SpannableStringParameterTypes.Color:
+                                    attributedString.AddAttribute(UIStringAttributeKey.ForegroundColor, Color.Parse(parameter.Value).Render(), range);
+                                    break;
+                                // TODO implement font faces.
+                                // case SpannableStringParameterTypes.Face: break;
+                                default: break;
+                            }
                         }
-                    }
-                    break;
-                default: break;
+                        break;
+                    default: break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.For(typeof(IOSRenderExtentions)).Error(ex, $"Id: {view?.Id.Or("null")}, Text: {view?.Text.Or("null")}, Range: {style?.Range}");
             }
         }
     }
